d6b311c63e6cbd1347883b40a3ceeec650a8ac8b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-31 09:15:29 -0300

    Initial commit

---
+++ Maioridade.py
@@ -0,0 +1,23 @@
+# Desigualdades: >, <, >=, <=, !=
+# Igualdade:
+# ==
+#
+# Comparacoes servem para caminhos SIM e NÃO, 0 e 1
+#
+# : (THEN) e ELSE:
+
+# titulo = "Maioridade"
+# print(f'{titulo:^30}')
+# idade = int(input("Que idade você tem?: "))
+# if idade >= 18: #sim, then, 1
+#     print("Você é maior de idade")
+# print("Obrigado por participar da pesquisa")
+
+titulo = "Maioridade"
+print(f'{titulo:^30}')
+idade = int(input("Que idade você tem?: "))
+if idade >= 18: #sim, then, 1
+    print("Você é maior de idade")
+else: #nao, else, 0
+    print("Você é menor de idade")
+print("Obrigado por participar da pesquisa")
